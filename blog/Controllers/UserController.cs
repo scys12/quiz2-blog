0207024eeca6662a6289880155c532908d43423f
Devi Hainun Pasya <61291198+notdevi@users.noreply.github.com>
2020-11-12 19:30:19 +0800

    add user controller

---
+++ blog/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using RegistrationAndLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class UserController : Controller
+    {
+        // Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        // Login Action
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // Log Out 
+        [Authorize]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "User");
+        }
+
+    }
+}
