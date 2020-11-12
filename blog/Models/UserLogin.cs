17efa333d9fa073e3f19659f8630c2d6d50724b7
Devi Hainun Pasya <61291198+notdevi@users.noreply.github.com>
2020-11-12 19:31:07 +0800

    add user models

---
+++ blog/Models/UserLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationAndLogin.Models
+{
+    public class UserLogin
+    {
+        [Display(Name = "Email")]
+        [Required(AllowEmptyStrings = false)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
